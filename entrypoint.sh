7da99377462eef67573bce929a677d677fcbf73d
Jarod Reid <fugu@hey.com>
2022-09-28 11:36:31 -0600

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-docker_run="docker run -d -p 6379:6379 redis:$INPUT_REDIS_VERSION redis-server --databases $INPUT_NUMBER_OF_DATABASES" --notify-keyspace-events 'AKE'
+docker_run="docker run -d -p 6379:6379 redis:$INPUT_REDIS_VERSION redis-server --databases $INPUT_NUMBER_OF_DATABASES --notify-keyspace-events 'AKE'"
 
 sh -c "$docker_run"
